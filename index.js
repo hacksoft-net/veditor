1da613ea6d71137e28dbf78a85b3eadbaea4bd20
hacksoft-net <enamvi@outlook.com>
2025-07-30 12:46:55 +0900

    Base

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+const main = (req, res) => {
+    let path = path.join(__dirname, "/pages/index.html");
+    res.sendFile(path);
+}
+
+app.get("/", main(req, res));
+app.use(express.static("public"));
+app.listen(port);
