876197469f6a9d3ec0d699514b44a904194eb190
hacksoft-net <enamvi@outlook.com>
2025-07-30 13:04:06 +0900

    OpenDG

---
+++ public/onclick.js
@@ -0,0 +1,7 @@
+let openDg = document.getElementById("open-dialog");
+let openDgBtn = document.getElementById("open-button");
+
+openDgBtn.addEventListener("onclick", () => {
+    openDg.showModal();
+});
+
